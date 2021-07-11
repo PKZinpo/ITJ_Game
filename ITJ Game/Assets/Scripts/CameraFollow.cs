4f6341c75f5715b9c2a0f5212673bccc0e1f67dc
PKZinpo <mbquach@gmail.com>
2021-07-11 00:50:28 -0500

    Lighting/Walls/Art

--- ITJ Game/Assets/Scripts/CameraFollow.cs
+++ ITJ Game/Assets/Scripts/CameraFollow.cs
@@ -1,35 +1,53 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour {
 
+    public Transform levelOne;
     public Transform player;
     public Transform bigSamurai;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
+    public Vector3 levelOneOffset;
 
+    private Scene currentScene;
     private Transform target;
 
+    void Start() {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
     void Update() {
-        if (target == bigSamurai) {
-            Vector3 desiredPos = target.position + offset;
-            Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
-            transform.position = smoothedPos;
+        if (currentScene.name != "Level1") {
+            if (target == bigSamurai) {
+                Vector3 desiredPos = target.position + offset;
+                Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+                transform.position = smoothedPos;
+            }
         }
     }
 
     void FixedUpdate() {
-        if (BigSamurai.isTouching) {
-            target = bigSamurai;
-            smoothSpeed = 0.01f;
+        if (currentScene.name == "Level1") {
+            Vector3 targetPos = levelOne.transform.position + levelOneOffset;
+            if (transform.position != targetPos) {
+                transform.position = targetPos;
+            }
         }
         else {
-            target = player;
-            smoothSpeed = 0.125f;
-        }
-        if (target == player) {
-            Vector3 desiredPos = target.position + offset;
-            Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
-            transform.position = smoothedPos;
+            if (BigSamurai.isTouching) {
+                target = bigSamurai;
+                smoothSpeed = 0.01f;
+            }
+            else {
+                target = player;
+                smoothSpeed = 0.125f;
+            }
+            if (target == player) {
+                Vector3 desiredPos = target.position + offset;
+                Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+                transform.position = smoothedPos;
+            }
         }
     }
 }
